7379bca29b84091f62f2d3116a1e688a03dbdf5f
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-05-14 15:10:23 +0300

    seekthermal: Add nodelet implementation

--- src/seekthermal_ros_nodelet.cpp
+++ src/seekthermal_ros_nodelet.cpp
@@ -1,4 +1,44 @@
 #include "seekthermal_camera/seekthermal_ros.hpp"
 #include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include <thread>
+
+namespace seekthermal_camera
+{
+
+class Camera : public nodelet::Nodelet
+{
+private:
+    std::unique_ptr<SeekThermalRos> instance;
+    std::thread pubThread;
+    std::atomic<bool> shouldStop;
+
+    void pubThreadFunc()
+    {
+        SeekThermalRos camera_connection(getName(), getNodeHandle(), getPrivateNodeHandle());
+        while (!shouldStop)
+        {
+            if(!camera_connection.publish())
+            {
+                // TODO: do something about that
+            }
+        }
+    }
+
+    void onInit() override
+    {
+        shouldStop = false;
+        pubThread = std::thread(&Camera::pubThreadFunc, this);
+    }
+
+    ~Camera()
+    {
+        shouldStop = true;
+        pubThread.join();
+    }
+};
+
+}
 
 
+PLUGINLIB_EXPORT_CLASS(seekthermal_camera::Camera, nodelet::Nodelet)
